e153894590486db40e99173009c06f78888f40cc
Warlock <internalmike@gmail.com>
2023-01-10 14:43:47 +0300

    ci

---
+++ ensure_no_std/src/main.rs
@@ -0,0 +1,38 @@
+#![feature(start)]
+
+#![deny(warnings)]
+
+#![no_std]
+
+#[cfg(windows)]
+#[link(name="msvcrt")]
+extern { }
+
+mod no_std {
+    use core::panic::PanicInfo;
+    use exit_no_std::exit;
+
+    #[panic_handler]
+    extern fn panic(_info: &PanicInfo) -> ! {
+        exit(99)
+    }
+}
+
+use arraybox::{ArrayBox, BufFor};
+
+trait Int {
+    fn value(&self) -> i32;
+}
+
+struct IntValue(i32);
+
+impl Int for IntValue {
+    fn value(&self) -> i32 { self.0 }
+}
+
+#[start]
+pub fn main(_argc: isize, _argv: *const *const u8) -> isize {
+    let a: ArrayBox<'static, dyn Int, BufFor<IntValue>> = ArrayBox::new(IntValue(7i32));
+    assert_eq!(a.value(), 7i32);
+    0
+}
