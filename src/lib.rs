e4a8c5153cb8978d7dc49d517126e884b562c3d4
Warlock <internalmike@gmail.com>
2024-09-10 21:17:55 +0300

    improve

--- src/lib.rs
+++ src/lib.rs
@@ -14,13 +14,7 @@ use core::mem::{ManuallyDrop, MaybeUninit, align_of, size_of};
 use core::ops::{Deref, DerefMut};
 use core::ptr::{self, Pointee};
 
-/// Stack-allocated space appropriated to store the specific type.
-///
-/// Appropriated for emplacing types with
-/// size less or equal `size_of::<T>()` and alignment less or equal `align_of::<T>()`.
-pub struct BufFor<T>(MaybeUninit<T>);
-
-/// A helper type for creating [`BufFor`] any of two types.
+/// A helper type for creating [`ArrayBox`] with buffer appropriated for any of two types.
 ///
 /// The type satisfies the following properties:
 ///
@@ -35,8 +29,10 @@ pub union AnyOf2<T1, T2> {
 }
 
 /// A stack-allocated container that can store dynamically sized types.
+///
+/// `B` type parameter specifies buffer size (`== size_of::<B>()`), and alignment (`== align_of::<B>()`).
 pub struct ArrayBox<'a, T: ?Sized + 'a, B> {
-    buf: BufFor<B>,
+    buf: MaybeUninit<B>,
     metadata: <T as Pointee>::Metadata,
     phantom: PhantomData<&'a T>,
 }
@@ -53,21 +49,21 @@ impl<'a, T: ?Sized + 'a, B> ArrayBox<'a, T, B> {
         assert!(align_of::<B>() >= align_of::<S>());
         assert!(size_of::<B>() >= size_of::<S>());
         let metadata = (&mut source as *mut T).to_raw_parts().1;
-        let mut res = ArrayBox { buf: BufFor(MaybeUninit::uninit()), metadata, phantom: PhantomData };
-        unsafe { ptr::write::<S>(res.buf.0.as_mut_ptr() as *mut u8 as *mut S, source) };
+        let mut res = ArrayBox { buf: MaybeUninit::uninit(), metadata, phantom: PhantomData };
+        unsafe { ptr::write::<S>(res.buf.as_mut_ptr() as *mut u8 as *mut S, source) };
         res
     }
 
     /// Return raw immutable pointer to the stored object.
     pub fn as_ptr(&self) -> *const T {
         let metadata = self.metadata;
-        ptr::from_raw_parts(self.buf.0.as_ptr() as *const u8 as *const (), metadata)
+        ptr::from_raw_parts(self.buf.as_ptr() as *const u8 as *const (), metadata)
     }
 
     /// Return raw mutable pointer to the stored object.
     pub fn as_mut_ptr(&mut self) -> *mut T {
         let metadata = self.metadata;
-        ptr::from_raw_parts_mut(self.buf.0.as_mut_ptr() as *mut u8 as *mut (), metadata)
+        ptr::from_raw_parts_mut(self.buf.as_mut_ptr() as *mut u8 as *mut (), metadata)
     }
 }
 
